8f0c102340543dccc4260ac5de2061aef155ae6a
Xylome <xylome@xavierheroult.net>
2014-10-08 16:40:19 +0200

    Using function.
      get_fourty_two() created

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+
+
+int get_fourty_two() {
+  return 42;
+}
+
+
+
 int main(int argc, char **argv) {
-  int fourty_two = 42;
+  int fourty_two;
+
+  fourty_two = get_fourty_two();
 
   printf("Hello git %d", fourty_two);
   
